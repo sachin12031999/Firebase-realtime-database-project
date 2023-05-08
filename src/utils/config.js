e467ab9814ccc5d0ff9b0ead9fa89a641577b72a
sachin12031999 <sachingurh@example.com>
2023-05-09 03:12:38 +0530

    update

---
+++ src/utils/config.js
@@ -0,0 +1,11 @@
+//TODO: DONE add firebase configuration and export it
+export const firebaseConfig = {
+  //TODO: ADD YOUR FIREBASE INFO HERE
+};
+//image configuration
+export const imageConfig = {
+  quality: 0.2,
+  maxWidth: 800,
+  maxHeight: 600,
+  autoRotate: true
+};
